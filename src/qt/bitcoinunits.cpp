4601e7da7840a8466d9a2237c604e98cb475c476
luke <luke@debian2.workgroup>
2014-10-14 11:16:56 +0100

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("GEO");
-    case mBTC: return QString("mGEO");
-    case uBTC: return QString::fromUtf8("μGEO");
+    case BTC: return QString("FIN");
+    case mBTC: return QString("mFIN");
+    case uBTC: return QString::fromUtf8("μFIN");
     default: return QString("???");
     }
 }
